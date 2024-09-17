1ea66bd1abb60a416e8fe24736dfe1e2b757614a
Saumyajit Chowdhury <saumyajit.c@gmail.com>
2024-09-17 00:09:51 -0400

    Added Header file

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 export default () => {
     
     return (
-        <div>
-            <h1>Hi there!</h1>
-            <hr />
+        <BrowserRouter>
+            <Header />
             <MarketingApp />
-        </div>
+        </BrowserRouter>
     );
 }
