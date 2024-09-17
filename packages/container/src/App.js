e8e729ea765c81fd93e70c406e5941fb42170943
Saumyajit Chowdhury <saumyajit.c@gmail.com>
2024-09-16 22:40:46 -0400

    Modified container.yml file

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
-    console.log("test load")
+    
     return (
         <div>
             <h1>Hi there!</h1>
