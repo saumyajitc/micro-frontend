28efba5e8b1b0006682d58dd90220b64d51e4b52
Saumyajit Chowdhury <saumyajit.c@gmail.com>
2024-09-17 18:08:07 -0400

    Fixed redirect library mapping

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,11 @@
 import React, { lazy, Suspense, useState, useEffect} from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 import { createBrowserHistory } from 'history';
 
 
 import Header from './components/Header';
 import Progress from './components/Progress';
-import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 const AuthLazy = lazy(() => import('./components/AuthApp'));
 const DashboardLazy = lazy(() => import('./components/DashboardApp'));
